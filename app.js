69a9bdd742a3fbd068c55414a8abf8e62b29cfdb
El3omda <52126083+El3omda@users.noreply.github.com>
2023-07-21 17:22:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+const { Client, MessageMedia, LocalAuth } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fileUpload = require('express-fileupload');
+const axios = require('axios');
+const mime = require('mime-types');
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+/**
+ * BASED ON MANY QUESTIONS
+ * Actually ready mentioned on the tutorials
+ * 
+ * Many people confused about the warning for file-upload
+ * So, we just disabling the debug for simplicity.
+ */
+app.use(fileUpload({
+  debug: false
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  authStrategy: new LocalAuth()
+});
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'يتم الاتصال . . .');
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'من فضلك قم بمسح هذا الرمز');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'البوت جاهز للأستخدام');
+    socket.emit('message', 'البوت جاهز للأستخدام');
+  });
+
+  client.on('authenticated', () => {
+    socket.emit('authenticated', 'تم الاتصال بنجاح');
+    socket.emit('message', 'تم الاتصال بنجاح');
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'فشل الاتصال تتم إعادة المحاولة . . .');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'تم قطع الاتصال');
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+app.get('/sendMessage', async (req, res) => {
+  const number = phoneNumberFormatter(req.query.number);
+  const message = req.query.message;
+
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({
+      status: false,
+      message: 'The number is not registered'
+    });
+  }
+
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err
+    });
+  });
+});
+
+server.listen(port, function() {
+  console.log('App running on *: ' + port);
+});
